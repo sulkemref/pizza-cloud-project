7c3050dcccfac44f4da8808fe19b92f9ade127a5
Kemail-terminal <sulkemref@gmail.com>
2023-03-15 13:43:16 -0500

    all-fix-1

--- src/main/java/com/cydeo/repository/PizzaRepository.java
+++ src/main/java/com/cydeo/repository/PizzaRepository.java
@@ -1,10 +1,12 @@
 package com.cydeo.repository;
 
 import com.cydeo.model.Pizza;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class PizzaRepository {
 
     private static List<Pizza> pizzaList = new ArrayList<>();
